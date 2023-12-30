3718b035f903de3c8dc98153729fe74ac31e6fd4
Erhan Bagdemir <erhan@reevik.net>
2023-12-30 15:35:36 +0100

    Javadocs as github pages

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.reevik.darkest"},{"l":"net.reevik.darkest.criteria"},{"l":"net.reevik.darkest.validators"}];updateSearchResults();
